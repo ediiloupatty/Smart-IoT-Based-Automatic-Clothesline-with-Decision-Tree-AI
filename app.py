1ba0adea65ccd26b38dad944fec323b3453a9776
edilouaptty <ediloupatty@gmail.com>
2025-05-19 18:12:42 +0800

    penambahan route ke falcon

--- app.py
+++ app.py
@@ -126,6 +126,11 @@ def nodemcu_reader():
 # =============================================
 # FLASK ROUTES
 # =============================================
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 def index():
     return render_template('index.html')
